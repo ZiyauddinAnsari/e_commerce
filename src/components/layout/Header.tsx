cbc008bf1d3d8622f405378d098561de168ef317
GATEWAYGROUP\Ziyauddin.Ansari <ziyauddin.ansari@thegatewaydigital.com>
2025-09-16 12:39:38 +0530

    Update logo name in Header component from "ShopVibe" to "Ziya"

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
               <ShoppingCart className="h-6 w-6 text-white" />
             </motion.div>
             <span className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-              ShopVibe
+              Ziya
             </span>
           </Link>
 
